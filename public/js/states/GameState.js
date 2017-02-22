a5e218609a57379b53755a8cb2cb8d813fd8c57c
Francesca Garbagnati <f.g.garbagnati@gmail.com>
2017-02-21 21:20:10 -0800

    Preloads images as sprites/spritesheets

--- public/js/states/GameState.js
+++ public/js/states/GameState.js
@@ -5,12 +5,19 @@ SpaceHipster.GameState = {
 
 	},
 	preload: function() {
+		this.load.image('space', 'assets/images/space.png');
+		this.load.image('player', 'assets/images/player.png');
+		this.load.image('bullet', 'assets/images/bullet.png');
+		this.load.image('enemyParticle', 'assets/images/yellow_enemy.png');
+		this.load.spritesheet('yellowEnemy', 'assets/images/yellow_enemy.png', 50, 46, 3, 1, 1);
+		this.load.spritesheet('redEnemy', 'assets/images/red_enemy.png', 50, 46, 3, 1, 1);
+		this.load.spritesheet('greenEnemy', 'assets/images/green_enemy.png', 50, 46, 3, 1, 1);
 
 	},
 	create: function() {
 
 	},
 	update: function() {
-		
+
 	}
 }
